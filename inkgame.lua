8697c760f9fe664269166a27bc841e8022bad5f6
TeamNico58 <108380973+TeamNico58@users.noreply.github.com>
2025-07-20 10:51:10 -0400

    Update inkgame.lua

--- inkgame.lua
+++ inkgame.lua
@@ -4,4 +4,3 @@
 pcall(function() delfile('284c7c5eb4a430a82162018c617e9aa0-cache.lua') end)
   local a pcall(function()a=readfile("static_content_130525/initv4.lua")end) if a and #a>2000 then a=loadstring(a) end;
 if a then return a() else pcall(makefolder, "static_content_130525") a=game:HttpGet("https://cdn.luarmor.net/v4_init_may312.lua") writefile("static_content_130525/initv4.lua", a) pcall(delfile, "static_content_130525/init.lua"); pcall(delfile, "static_content_130525/initv2.lua"); pcall(delfile, "static_content_130525/initv3.lua"); loadstring(a)() end
-  
